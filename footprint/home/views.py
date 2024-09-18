94493c3d8e1374ff3219460d2d4ea3e9f4f9044b
akram213103 <akram213103@gmail.com>
2024-09-18 17:36:48 -0400

    Signup page updates
    - added email requirements
    - added password requirements and validation
    - emails and their hashed passwords get sent to database

--- footprint/home/views.py
+++ footprint/home/views.py
@@ -21,7 +21,31 @@ def login_view(request):
     
     return render(request, 'home/login.html')
 
+# validating passwords
+def validate_password(password):
+    if len(password) < 8 or not any(c.islower() for c in password) or not any(c.isupper() for c in password):
+        return False
+    return True
+
+
 #new fucntion needed for registering
 
 def signup_view(request):
-    return render(request, 'home/signup.html')  # Render the sign-up template
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # Validate password
+        if not validate_password(password):
+            return render(request, 'home/signup.html', {
+                'invalid_password_message': 'Password does not meet the requirements.',
+            })
+
+        try:
+            # Firebase Admin SDK: create user
+            user = auth.create_user(email=email, password=password)
+            messages.success(request, 'User created successfully!')
+            return redirect('login')
+        except Exception as e:
+            messages.error(request, f"Error creating user: {str(e)}")
+    return render(request, 'home/signup.html')
