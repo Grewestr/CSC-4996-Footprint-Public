0bd14eba1cacd80f003c0e577a302c2e3753a66e
Timothy M Kosinski <Gx5452@wayne.edu>
2024-11-12 22:18:25 -0500

    updated the versions for the AI models. Image_classifcation.py

--- footprint/home/static/AI_Scripts/Image_classification.py
+++ footprint/home/static/AI_Scripts/Image_classification.py
@@ -32,7 +32,7 @@ def detect_clothing_attributes(person_crop):
     _, img_encoded = cv2.imencode(".jpg", person_crop)
     img_base64 = base64.b64encode(img_encoded).decode("utf-8")
     try:
-        result = CLIENT.infer(img_base64, model_id="clothing-detection-2/6")
+        result = CLIENT.infer(img_base64, model_id="clothing-detection-2/9")
         return result['predictions']
     except Exception as e:
         print(f"Inference for clothing attributes failed: {e}")
@@ -43,7 +43,7 @@ def detect_top_color(person_crop):
     _, img_encoded = cv2.imencode(".jpg", person_crop)
     img_base64 = base64.b64encode(img_encoded).decode("utf-8")
     try:
-        result = CLIENT.infer(img_base64, model_id="hair_color_detection/5")
+        result = CLIENT.infer(img_base64, model_id="hair_color_detection/7")
         return result['predictions']
     except Exception as e:
         print(f"Inference for top color failed: {e}")
